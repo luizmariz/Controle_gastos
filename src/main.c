553a78666e13cc923fe04ef8d823587b7d1dccea
Luiz <37978726+luizmariz@users.noreply.github.com>
2018-06-24 15:34:53 -0300

    -

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ o funcionamento eh semelhante a um callback em que a interacao do usuario funcio
 #include <stdio.h>
 #include <gtk/gtk.h>
 #include <time.h>
+#include <string.h>
 
 GtkWidget *g_lbl_receita;
 GtkWidget *g_lbl_orcamento;
@@ -32,13 +33,80 @@ GtkWidget *g_bar_orcamento;
 
 GtkCalendar *g_cal_calendario;
 
+typedef struct{
+    char descricao[40];
+    char conta[20];
+    double valor; 
+}entrada;
 
-FILE *p_main_file;
+typedef struct{
+    char descricao[40];
+    double valor;
+    double progresso;
+}desejo;
 
+typedef struct{
+    double um;
+    double dois;
+    double carteira;
+    double met;
+    double despesas;
+    double status_met;
+    double orcament;
+    double status_orc;
 
-int main(int argc, char *argv[]) 
-{
-    //
+}saldo_contas;
+
+saldo_contas saldo;
+
+void atualizar_saldo(char *conta, double valor){
+    
+    if (strcmp(conta, "Conta1") == 0){
+        saldo.um = saldo.um + valor;
+    }
+
+    if (strcmp(conta, "Conta2") == 0){
+        saldo.dois = saldo.dois + valor;
+    }
+
+    if (strcmp(conta, "Carteira") == 0){
+        saldo.carteira = saldo.carteira + valor;
+    }
+
+    if (strcmp(conta, "Meta") == 0){
+        saldo.met = saldo.met + valor;
+    }
+    
+}// essa funcao evita a repeticao dos if ao longo do programa
+
+void update(int comando){ // 0 - ler, 1 - escrever
+
+    FILE *p_bin = fopen("main.dat","ab+");
+
+    if(comando == 0){
+        if(fread(&saldo, sizeof(saldo_contas), 1, p_bin) == 0){
+       
+            saldo.um = 0;
+            saldo.dois = 0;
+            saldo.carteira = 0;
+            saldo.met = 0;
+            saldo.despesas = 0;
+            saldo.status_met = 0;
+            saldo.orcament = 0;
+            saldo.status_orc = 0;
+
+            fwrite(&saldo, sizeof(saldo_contas), 1, p_bin);
+        }
+    }
+
+    if(comando == 1){
+        fwrite(&saldo, sizeof(saldo_contas), 1, p_bin);
+    }
+
+    fclose(p_bin);
+}
+
+int main(int argc, char *argv[]){
     
     GtkBuilder  *builder; 
     GtkWidget   *window; 
@@ -51,9 +119,6 @@ int main(int argc, char *argv[])
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
     gtk_builder_connect_signals(builder, NULL);
     
-    //
-
-    p_main_file = fopen("data/main.txt","r+");
 
     //ponteiros para a GUI
     
@@ -97,71 +162,101 @@ int main(int argc, char *argv[])
 
     return 0;
 }
- 
-// funcoes - a destroy eh chamda qndo a janela e fechada
 
 /* a funcao sprintf funciona de forma analoga a printf com a diferenca de que a sua resultante
 sera colocada em uma string - sprintf(str, "", variavel) */
 
+// encerrar programa  -- gtk_main_quit() - encerra a interface grafica
 // pegar valor        -- gtk gtk_spin_button_get_value(GTK_SPIN_BUTTON(ponteiro)) - retorna valor
 // pegar texto        -- gtk_entry_get_text(GTK_ENTRY(ponteiro))) - retorna ponteiro para o texto
 // selecionar a conta -- gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(ponteiro)) - retorna a combobox selecionada
 // controle progresso -- gtk_progress_bar_set_fraction(ponteiro, fracao double de 0.0 a 1.0) - retorna a apresentacao da barra
 // pegar data         -- gtk_calendar_get_date (ponteiro, ponteiro ano, ponteiro mes, ponteiro dia);
 
-/* exemplos  
-
+void pegar_dia(char *da){
+    
     unsigned int d,m,a;
     unsigned int *dia, *ano, *mes;
 
     dia = &d;
     mes = &m;
     ano = &a;
-    
+
     gtk_calendar_get_date (g_cal_calendario, ano, mes, dia);
+
+    sprintf(da,"%d-%d-%d",d,m,a);
+
+}
+
+void on_Nova_receita_clicked(){
     
-    printf("%s", gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(g_comb_contas)));
-    gtk_label_set_text(GTK_LABEL(g_lbl_receita), saida_str);
-    printf("%s", gtk_entry_get_text(GTK_ENTRY(g_etr_entrada1)));
-    gtk_progress_bar_set_fraction(g_bar_meta, quantidade);
- */
+    entrada receita;
 
-void on_Nova_receita_clicked()
-{
-    double quantidade = 0; 
-    quantidade = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor1));
- 
+    strcpy(receita.conta , gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(g_comb_contas)));
+
+    char nome_arquivo_txt[25] = "data/historico/";
+    char data[12];
+
+    pegar_dia(data);
+    strcat(nome_arquivo_txt, data);
+
+    FILE *p_historico = fopen(nome_arquivo_txt, "a");
+
+    receita.valor = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor1));
+    strcpy(receita.descricao , gtk_entry_get_text(GTK_ENTRY(g_etr_entrada1)));
+
+    fprintf(p_historico,"\n%s: +R$%.2f %s", receita.conta, receita.valor, receita.descricao);
+
+    fclose(p_historico);
+
+    atualizar_saldo(receita.conta, receita.valor);
+    
 }
 
-void on_Nova_despesa_clicked()
-{
-    double quantidade = 0; 
-    quantidade = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor1));
+void on_Nova_despesa_clicked(){
+    
+    entrada despesa;
+
+    strcpy(despesa.conta , gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(g_comb_contas)));
+
+    char nome_arquivo_txt[25] = "data/historico/";
+    char data[12];
+
+    pegar_dia(data);
+    strcat(nome_arquivo_txt, data);
+
+    FILE *p_historico = fopen(nome_arquivo_txt, "a");
+
+    despesa.valor = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor1));
+    strcpy(despesa.descricao , gtk_entry_get_text(GTK_ENTRY(g_etr_entrada1)));
+
+    fprintf(p_historico,"\n%s: -R$%.2f %s", despesa.conta, despesa.valor, despesa.descricao);
+
+    fclose(p_historico);
+
+    atualizar_saldo(despesa.conta, despesa.valor);
+    saldo.despesas = saldo.despesas + despesa.valor;
+
 }
  
-void on_Nova_meta_clicked()
-{
+void on_Nova_meta_clicked(){
     double quantidade = 0; 
     quantidade = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor2));
 }
 
-void on_Novo_orcamento_clicked()
-{
+void on_Novo_orcamento_clicked(){
     double quantidade = 0; 
     quantidade = gtk_spin_button_get_value(GTK_SPIN_BUTTON(g_spbtn_valor2));   
 }
 
-void on_calendario_day_selected()
-{
+void on_calendario_day_selected(){
 
 }
 
-void on_Update_clicked()
-{
+void on_reajuste_clicked(){
 
 }
 
-void on_window_main_destroy()
-{
+void on_window_main_destroy(){
     gtk_main_quit();
 }
